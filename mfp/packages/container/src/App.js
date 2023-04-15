75142aecf258f0d4bb5a97e473dbe4f7bd2dfe13
Surendhar D(surendhar.d) <surendhar.d@infosys.com>
2023-04-15 11:10:36 +0530

    Initial setup changes

---
+++ mfp/packages/container/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
+import MarketingApp from "./components/MarketingApp";
+
+export default () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
+  );
+};
